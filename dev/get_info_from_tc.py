7937539eed3070ed98719435d833946606570570
Igor <igorlachman@gmail.com>
2020-08-28 18:49:18 +0300

    udate_v_last

--- dev/get_info_from_tc.py
+++ dev/get_info_from_tc.py
@@ -7,6 +7,7 @@ teamcity_build_id_of_first_job = sys.argv[1]
 login_TCA = TCA(conf.tc_login, conf.tc_password, conf.tc_host)
 parameters_content = login_TCA.get_properties_from_job_by_build_id(teamcity_build_id_of_first_job)
 random_env_from_first_job = DataProcess.get_env_parameters_of_job_by_build_id(parameters_content, 'env.rand')
-print(random_env)
+print(random_env_from_first_job)
+
 
 
